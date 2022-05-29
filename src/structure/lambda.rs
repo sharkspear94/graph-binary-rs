70751449a7ddca43c5f000143d5c6383ad536f60
felix <felix.kuhnkuhn@gmail.com>
2022-05-29 21:26:07 +0200

    added De-Encode  impls for Lambda

--- src/structure/lambda.rs
+++ src/structure/lambda.rs
@@ -1,7 +1,57 @@
-use crate::graph_binary::GraphBinary;
+use crate::{
+    graph_binary::{Decode, Encode, GraphBinary},
+    specs::CoreType,
+    struct_de_serialize,
+};
 
 #[derive(Debug, PartialEq)]
 pub struct Lambda {
     language: String,
-    values: Vec<GraphBinary>,
+    script: String,
+    arguments_length: i32,
 }
+
+impl Encode for Lambda {
+    fn type_code() -> u8 {
+        CoreType::Lambda.into()
+    }
+
+    fn write_patial_bytes<W: std::io::Write>(
+        &self,
+        writer: &mut W,
+    ) -> Result<(), crate::error::EncodeError> {
+        self.language.write_patial_bytes(writer)?;
+        self.script.write_patial_bytes(writer)?;
+        self.arguments_length.write_patial_bytes(writer)
+    }
+}
+
+impl Decode for Lambda {
+    fn expected_type_code() -> u8 {
+        CoreType::Lambda.into()
+    }
+
+    fn partial_decode<R: std::io::Read>(reader: &mut R) -> Result<Self, crate::error::DecodeError>
+    where
+        Self: std::marker::Sized,
+    {
+        let language = String::partial_decode(reader)?;
+        let script = String::partial_decode(reader)?;
+        let arguments_length = i32::partial_decode(reader)?;
+
+        Ok(Lambda {
+            language,
+            script,
+            arguments_length,
+        })
+    }
+
+    fn partial_count_bytes(bytes: &[u8]) -> Result<usize, crate::error::DecodeError> {
+        let mut len = String::partial_count_bytes(bytes)?;
+        len += String::partial_count_bytes(&bytes[len..])?;
+        len += i32::partial_count_bytes(&bytes[len..])?;
+        Ok(len)
+    }
+}
+
+struct_de_serialize!((Lambda, LambdaVisitor, 254));
